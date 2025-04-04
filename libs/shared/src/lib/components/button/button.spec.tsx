4feb949215f54301c9a60e8099ee9b2681eedacc
Daniela Farias <danielacfp.web@gmail.com>
2025-04-04 01:03:02 -0300

    Update button.spec.tsx

--- libs/shared/src/lib/components/button/button.spec.tsx
+++ libs/shared/src/lib/components/button/button.spec.tsx
@@ -5,7 +5,7 @@ import { BytebankButton } from './index';
 describe('Button', () => {
   it('should render successfully', () => {
     const { baseElement } = render(
-      <BytebankButton text="Concluir transação" type="ORANGE" outlined={true} />
+      <BytebankButton text="Concluir transação" type="GREEN" outlined={true} />
     );
     expect(baseElement).toBeTruthy();
   });
