c7df7a401a6b5876006abc8fcbb3d6e32d5863cd
daniela pereira <danielacfp.web@gmail.com>
2025-04-08 20:01:36 -0300

    fix: configured libs to execute unit tests and fixed tests

--- libs/shared/src/lib/components/input/input.spec.tsx
+++ libs/shared/src/lib/components/input/input.spec.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BytebankInput } from './ControlledInput';
+import { useForm, FormProvider } from 'react-hook-form';
+
+const Wrapper = ({ children }) => {
+    const methods = useForm();
+
+    return <FormProvider {...methods}>{children}</FormProvider>;
+};
 
 describe('Input', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<BytebankInput name="nome" label="nome" />);
-    expect(baseElement).toBeTruthy();
-  });
+    it('render bytebankInput component correctly', () => {
+        const { baseElement } = render(<Wrapper><BytebankInput name="nome" label="nome"  /></Wrapper>);
+        expect(baseElement).toBeDefined();
+    });
 });
