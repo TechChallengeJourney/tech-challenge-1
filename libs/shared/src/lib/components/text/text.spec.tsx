e146ff3d097e473b3fdb194fb817aa4469818f73
EloisaFagundes <lola.fernanda@hotmail.com>
2025-04-09 15:15:47 -0300

    test: to render label

--- libs/shared/src/lib/components/text/text.spec.tsx
+++ libs/shared/src/lib/components/text/text.spec.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { BytebankText } from './index';
 
-describe('Button', () => {
-  it('should render successfully text component', () => {
+describe('BytebankText', () => {
+  it('should render the label correctly', () => {
     const label = "The quick brown fox jumps over the lazy dog";
     const color = "primary";
     const type = "md";
 
-    const { baseElement } = render(
-      <BytebankText label={label} color={color} type={type}/>
-    );
-    expect(baseElement).toBeTruthy();
+    render(<BytebankText label={label} color={color} type={type} />);
+
+    const textElement = screen.getByText(label);
+    expect(textElement).toBeInTheDocument();
   });
 });
