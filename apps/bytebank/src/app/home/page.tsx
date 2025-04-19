cf125924c83fcdb2ea92553a298bdeee015181dd
EloisaFagundes <lola.fernanda@hotmail.com>
2025-04-18 21:48:55 -0300

    refator: improve media queries props

--- apps/bytebank/src/app/home/page.tsx
+++ apps/bytebank/src/app/home/page.tsx
@@ -1,6 +1,11 @@
 'use client';
 import { BytebankText, BytebankIllustration } from '@bytebank/shared';
-import CardGiftcardOutlined from '@mui/icons-material/CardGiftcardOutlined';
+import {
+  CardGiftcardOutlined,
+  AssuredWorkload,
+  StarBorderOutlined,
+  DevicesOtherOutlined,
+} from '@mui/icons-material';
 import styles from './page.module.scss';
 
 export default function Home() {
@@ -11,7 +16,11 @@ export default function Home() {
           Experimente mais liberdade no controle da sua vida financeira. Crie
           sua conta com a gente!
         </BytebankText>
-        <BytebankIllustration className={styles.illustration} variant='auto' name="graphic"/>
+        <BytebankIllustration
+          className={styles.illustration}
+          variant="auto"
+          name="graphic"
+        />
       </div>
     );
   };
@@ -19,34 +28,35 @@ export default function Home() {
   const renderValuePropositionBlock = () => {
     return (
       <div className={styles.valuePropositionWrapper}>
-        
         <div className={styles.valueProposition}>
           <CardGiftcardOutlined color="success" sx={{ fontSize: 80 }} />
-          <BytebankText>Conta e cartão gratuitos</BytebankText>
-          <BytebankText>
+          <BytebankText color="success">Conta e cartão gratuitos</BytebankText>
+          <BytebankText className={styles.text}>
             Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso:
             sem tarifa de manutenção.
           </BytebankText>
         </div>
         <div className={styles.valueProposition}>
-          <CardGiftcardOutlined color="success" sx={{ fontSize: 80 }} />
-          <BytebankText>Saques sem custo</BytebankText>
-          <BytebankText>
+          <AssuredWorkload color="success" sx={{ fontSize: 80 }} />
+          <BytebankText color="success">Saques sem custo</BytebankText>
+          <BytebankText className={styles.text}>
             Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.
           </BytebankText>
         </div>
         <div className={styles.valueProposition}>
-          <CardGiftcardOutlined color="success" sx={{ fontSize: 80 }} />
-          <BytebankText>Saques sem custo</BytebankText>
-          <BytebankText>
-            Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.
+          <StarBorderOutlined color="success" sx={{ fontSize: 80 }} />
+          <BytebankText color="success">Programa de pontos</BytebankText>
+          <BytebankText className={styles.text}>
+            Você pode acumular pontos com suas compras no crédito sem pagar
+            mensalidade!
           </BytebankText>
         </div>
         <div className={styles.valueProposition}>
-          <CardGiftcardOutlined color="success" sx={{ fontSize: 80 }} />
-          <BytebankText>Saques sem custo</BytebankText>
-          <BytebankText>
-            Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.
+          <DevicesOtherOutlined color="success" sx={{ fontSize: 80 }} />
+          <BytebankText color="success">Seguro Dispositivos</BytebankText>
+          <BytebankText className={styles.text}>
+            Seus dispositivos móveis (computador e laptop) protegidos por uma
+            mensalidade simbólica.
           </BytebankText>
         </div>
       </div>
