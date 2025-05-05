d9d860965ccaa593b5af98afd451445a572a9806
Dimitri13Freitas <freitasdimitri7@gmail.com>
2025-05-05 19:12:36 -0300

    chore: bring latest changes

--- libs/shared/src/lib/components/card/index.tsx
+++ libs/shared/src/lib/components/card/index.tsx
@@ -2,7 +2,7 @@ import { Card } from '@mui/material';
 
 export interface BytebankCardProps {
   bgcolor?: string;
-  radius?: 'sm' |'md' | 'lg';
+  radius?: 'sm' | 'md' | 'lg';
   variant?: 'elevation' | 'outlined';
   className?: string;
   children?: React.ReactNode;
@@ -11,11 +11,21 @@ export interface BytebankCardProps {
 export enum BytebankCardRadius {
   sm = '5px',
   md = '10px',
-  lg = '14px'
+  lg = '14px',
 }
-export function BytebankCard({ bgcolor = '#FFF', radius = 'md', variant = 'outlined', children, className }: BytebankCardProps) {
+export function BytebankCard({
+  bgcolor = '#FFF',
+  radius = 'md',
+  variant = 'outlined',
+  children,
+  className,
+}: BytebankCardProps) {
   return (
-    <Card variant={variant} sx={{ 'background': bgcolor, 'borderRadius': BytebankCardRadius[radius] }} className={className}>
+    <Card
+      variant={variant}
+      sx={{ background: bgcolor, borderRadius: BytebankCardRadius[radius] }}
+      className={className}
+    >
       {children}
     </Card>
   );
