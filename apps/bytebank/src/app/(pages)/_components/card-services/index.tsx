8cd9ab85fb7e3bafe5788d4dfd1073766c9d8a37
Dimitri13Freitas <freitasdimitri7@gmail.com>
2025-05-18 18:41:30 -0300

    feat: creation of card-services component for page other services and fixed error of tsconfig on shared folder

---
+++ apps/bytebank/src/app/(pages)/_components/card-services/index.tsx
@@ -0,0 +1,40 @@
+import { Box } from '@mui/material';
+import { BytebankText, BytebankCard, palette } from '@bytebank/shared';
+import './style.scss';
+import '../card-transaction/style.scss';
+
+export function BytebankServices() {
+  return (
+    <BytebankCard
+      className="bytebank-card-content"
+      bgcolor={palette['grey.300']}
+    >
+      <Box p={4}>
+        <BytebankText variant="lg" color="black">
+          Confira os serviços disponíveis
+        </BytebankText>
+      </Box>
+
+      <Box className="servicesWrapper">
+        <Box sx={{ background: 'red' }} height={167}>
+          1
+        </Box>{' '}
+        <Box sx={{ background: 'red' }} height={167}>
+          1
+        </Box>{' '}
+        <Box sx={{ background: 'red' }} height={167}>
+          1
+        </Box>{' '}
+        <Box sx={{ background: 'red' }} height={167}>
+          1
+        </Box>{' '}
+        <Box sx={{ background: 'red' }} height={167}>
+          1
+        </Box>{' '}
+        <Box sx={{ background: 'red' }} height={167}>
+          1
+        </Box>
+      </Box>
+    </BytebankCard>
+  );
+}
--- apps/bytebank/src/app/(pages)/outros/page.tsx
+++ apps/bytebank/src/app/(pages)/outros/page.tsx
@@ -1,4 +1,17 @@
-export default function Outros() {
+'use client';
+import { useState } from 'react';
+import { BytebankBalanceCard } from '../_components/balance-card';
+import { BytebankExtractCard } from '../home/_components';
+import styles from './page.module.scss';
+import { BytebankServices } from '../_components/card-services';
 
-  return (<p>Outros</p>);
+export default function Outros() {
+  return (
+    <div className={styles.pageWrapper}>
+      <BytebankBalanceCard />
+      <BytebankExtractCard />
+
+      <BytebankServices />
+    </div>
+  );
 }
