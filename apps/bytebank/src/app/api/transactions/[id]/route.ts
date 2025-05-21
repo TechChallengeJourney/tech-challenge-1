aa957343cf4a7968ed3163f2d1f000d5eb52b366
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-05-21 09:29:19 -0300

    fix: path

--- apps/bytebank/src/app/api/transactions/[id]/route.ts
+++ apps/bytebank/src/app/api/transactions/[id]/route.ts
@@ -3,7 +3,7 @@ import {
   fetchTransactions,
   updateTransaction,
   deleteTransaction,
-} from '../../../services/transactions/transactions.service';
+} from '../../../services/extract/transactions.service';
 
 export async function GET(request: Request) {
   try {
