3690b16aa533a8ffb6087c638f13f872054edac0
EloisaFagundes <eloisa.fagundes@rdstation.com>
2025-05-27 22:07:54 -0300

    fix: refactoring to avoid loading in public paths

--- libs/shared/src/lib/contexts/user.context.tsx
+++ libs/shared/src/lib/contexts/user.context.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { User } from '../shared';
 import { useSession } from '../hooks/use-session';
@@ -6,6 +8,7 @@ interface UserContextProps {
   user: User | null;
   setUser: (user: User | null) => void;
   loading: boolean;
+  isAuthenticated: boolean;
 }
 
 const UserContext = createContext<UserContextProps | undefined>(undefined);
@@ -14,23 +17,33 @@ export const UserProvider: React.FC<{ children: React.ReactNode }> = ({ children
   const [sessionUser, setSessionUser] = useSession<User | null>('user');
   const [user, setUser] = useState<User | null>(sessionUser);
   const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!sessionUser);
+
+  // console.log(isAuthenticated, 'isAuthenticated');
+  // console.log(sessionUser, 'sessionUser in UserProvider');
 
   useEffect(() => {
     if (sessionUser !== null) {
+      console.log('sessionUser:  IF preenchido');
       setUser(sessionUser);
+      setIsAuthenticated(true);
     } else {
+      console.log('sessionUser:  ELSE null');
       setUser(null);
+      setIsAuthenticated(false);
     }
     setLoading(false);
   }, [sessionUser]);
 
   const handleSetUser = (user: User | null) => {
+    console.log('handleSetUser called with');
     setUser(user);
     setSessionUser(user);
+    setIsAuthenticated(!!user);
   };
 
   return (
-    <UserContext.Provider value={{ user, setUser: handleSetUser, loading }}>
+    <UserContext.Provider value={{ user, setUser: handleSetUser, loading, isAuthenticated }}>
       {children}
     </UserContext.Provider>
   );
